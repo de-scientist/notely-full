1c115e57b3da204b627d1915244646eeb6c94c2f
de-scientist <gitaumark502@gmail.com>
2025-12-09 16:36:38 +0300

    update routes in the Admin Routes

--- client/src/admin/AdminRoute.tsx
+++ client/src/admin/AdminRoute.tsx
@@ -8,9 +8,9 @@ export default function AdminRoute() {
   return (
     <Routes>
       <Route path="/admin" element={<AdminLayout />}>
-        <Route path="rag" element={<RAGUploader />} />
-        <Route path="queries" element={<QueriesTable />} />
-        <Route path="messages" element={<MessagesInbox />} />
+        <Route path="/rag" element={<RAGUploader />} />
+        <Route path="/queries" element={<QueriesTable />} />
+        <Route path="/messages" element={<MessagesInbox />} />
       </Route>
     </Routes>
   );
