4d136f7480c73529cfc863106518ee6e2cfb9611
de-scientist <gitaumark502@gmail.com>
2025-12-09 18:11:11 +0300

    comment out unused imports in the edit entry page

--- client/src/pages/EditEntryPage.tsx
+++ client/src/pages/EditEntryPage.tsx
@@ -16,7 +16,7 @@ import { Label } from '../components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../components/ui/select';
 import { Separator } from '../components/ui/separator';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../components/ui/tabs';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../components/ui/tooltip';
+//import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../components/ui/tooltip';
 import { Switch } from "../components/ui/switch";
 
 // Icons
