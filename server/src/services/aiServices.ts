a6af47d139a4ac094ec3319481091d988a5329be
de-scientist <gitaumark502@gmail.com>
2025-12-10 22:21:59 +0300

    add import dotenv from "dotenv"; dotenv.config();

--- server/src/services/aiServices.ts
+++ server/src/services/aiServices.ts
@@ -1,5 +1,8 @@
 // server/src/services/aiService.ts
 import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
