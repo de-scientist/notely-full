b5517012a441fc923d9157897671b8ffe8a18c66
de-scientist <gitaumark502@gmail.com>
2025-12-12 15:18:28 +0300

    export default function AuthCallback()

---
+++ client/src/pages/AuthCallback.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { supabase } from "../lib/supabaseClient";
+
+export default function AuthCallback() {
+  useEffect(() => {
+    async function check() {
+      const { data } = await supabase.auth.getUser();
+      if (data?.user) window.location.href = "/dashboard";
+      else window.location.href = "/login";
+    }
+    check();
+  }, []);
+
+  return <p>Finishing login…</p>;
+}
