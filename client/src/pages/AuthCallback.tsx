951a9646db32fb41f976bb904c404fb43ac94cf7
de-scientist <gitaumark502@gmail.com>
2025-12-12 15:21:14 +0300

    add import { supabase } from "../lib/supabase";

--- client/src/pages/AuthCallback.tsx
+++ client/src/pages/AuthCallback.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../lib/supabase";
 
 export default function AuthCallback() {
   useEffect(() => {
